25499bd676acc074c127ce5dcd34e77a97bb3fc9
OliviaZ <lz2933@columbia.edu>
2023-10-18 16:52:12 +0000

    for gcp

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from AcadMate, this is a greeting from EC2.'
+    return 'Hello from AcadMate, this is a greeting from GCP.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
